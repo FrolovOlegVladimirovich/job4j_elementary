4a5e66b98da817577f56be14345f79d7e1dd5292
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-05-05 18:04:18 +0300

    1.4. JUnit[#139244]

---
+++ src/test/java/ru/job4j/calculate/CalculateTest.java
@@ -0,0 +1,38 @@
+package ru.job4j.calculate;
+
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test
+ *
+ * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
+ * @version 1.0
+ * @since 05.05.2019
+ */
+public class CalculateTest {
+/**
+ * Test add
+ */
+    @Test
+    public void whenAddOneToOneThenTwo() {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+        Calculate.main(null);
+        assertThat(out.toString(), is("Hello world!\n"));
+    }
+/**
+ * Test echo
+ */
+    @Test
+    public void whenTakeNameThenThreeEchoPlusName() {
+        String input = "Oleg Frolov";
+        String expect = "Echo, echo, echo : Oleg Frolov";
+        Calculate calc = new Calculate();
+        String result = calc.echo(input);
+        assertThat(result, is(expect));
+    }
+}
---
+++ src/test/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
+* @version 1.0
+* @since 05.05.2019
+*/
+package ru.job4j.calculate;
