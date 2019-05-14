a54c0b4a55d519133c705649d84a270a589c64ba
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-05-14 18:34:07 +0300

    5.5. Рефакторинг кода.[#139207]

--- src/main/java/ru/job4j/loop/Paint.java
+++ src/main/java/ru/job4j/loop/Paint.java
@@ -1,10 +1,12 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
 /**
  * Рисуем пирамиду заданной высоты символами '^' и ' '.
  * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
  * @since 14.05.2019
- * @version 1.0
+ * @version 2.0
  */
 public class Paint {
 
@@ -18,53 +20,58 @@ public class Paint {
      * @param height Высота пирамиды.
      * @return Метод toString возвращает из объекта screen класса StringBuilder все добавленные в него символы и строки в одну строку.
      * В результате получаем нарисованную правую часть пирамиды.
+     *
+     * С помощью метода loopBy убираем лишнюю часть кода:
      */
     public String rightTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
     }
 
     /**
      * Рисуем левую часть пирамиды с помощью циклов (по аналогии с правой частью).
      *
+     * С помощью метода loopBy убираем лишнюю часть кода:
      */
     public String leftTrl(int height) {
-        StringBuilder screen = new StringBuilder();
-        int width = height;
-        for (int row = 0; row != height; row++) {
-            for (int column = 0; column != width; column++) {
-                if (row >= width - column - 1) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            screen.append(System.lineSeparator());
-        }
-        return screen.toString();
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
     }
 
     /**
      * Рисуем целую пирамиду.
      *
+     * С помощью метода loopBy убираем лишнюю часть кода:
      */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+
+    }
+
+    /**
+     * Метод для рефакторинга кода (с помощью данного метода убираем повторяющийся код в классе Paint.java).
+     * C использованием Generics и Lambda.
+     *
+     * @param height Высота пирамиды.
+     * @param height Ширина пирамиды/строки.
+     * @return Метод toString возвращает из объекта screen класса StringBuilder все добавленные в него символы и строки в одну строку.
+     * В результате получаем нарисованную правую часть пирамиды.
+     */
+    private String loopBy(int height, int width, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int width = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != width; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
