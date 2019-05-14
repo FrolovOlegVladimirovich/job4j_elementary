c51425b33425a8f5f7459d60d9f7fdb2f078206b
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-05-14 13:27:59 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне[#139210]

---
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -0,0 +1,29 @@
+package ru.job4j.loop;
+
+/**
+ * Расчет суммы четных числе в диапазоне.
+ * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
+ * @since 14.05.2019
+ * @version 1.0
+ */
+public class Counter {
+
+    /**
+     * Метод для расчета суммы четных числе в диапазоне.
+     *
+     * @param start первое значение диапазона.
+     * @param finish второе значение диапазона.
+     * @return cумма четных числе в диапазоне.
+     */
+    public int add(int start, int finish) {
+        int sum = 0;
+        if (start % 2 != 0) {
+            start++;
+        }
+
+        for (int i = start; i <= finish; i = i + 2) {
+            sum = sum + i;
+        }
+        return sum;
+    }
+}
---
+++ src/test/java/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,18 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class CounterTest {
+    @Test
+    /**
+     * Проверка суммы четных чисел в диапазоне от 1 до 10.
+     */
+    public void whenSumEvenNumbersFromOneToTenThenThirty() {
+        Counter count = new Counter();
+        int result = count.add(1, 10);
+        assertThat(result, is(30));
+    }
+}
