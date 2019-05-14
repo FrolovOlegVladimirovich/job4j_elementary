8c9e010e0c1517396788172a23aba04104941744
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-05-14 16:10:55 +0300

    5.3. Построить шахматную доску в псевдографике.[#139214]

---
+++ src/main/java/ru/job4j/loop/Board.java
@@ -0,0 +1,43 @@
+package ru.job4j.loop;
+
+/**
+ * Рисуем шахматную доску заданного размера символами 'x' и ' '.
+ * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
+ * @since 14.05.2019
+ * @version 1.0
+ */
+public class Board {
+
+    /**
+     * Рисуем шахматную доску с помощью циклов.
+     *
+     * Класс StringBuilder служит для построения строки.
+     * Метод append добавляет символы и строки в некоторый буфер StringBuilder вместо вывода на консоль.
+     *
+     * Цикл с итератором i - строка, с итератором j - столбец.
+     *
+     * Высота доски достигается за счёт переходов на новую линию. В Windows это "\r\n", в Linux - "\n".
+     * Универсальный вариант: System.getProperty("line.separator") - автоматически определяет переход в зависимости от ОС.
+     *
+     * screen.append(ln) - добавляет перевод на новую строку.
+     *
+     * @param width Ширина доски (столбец).
+     * @param height Высота доски (строка).
+     * @return Метод toString для объекта screen (StringBuilder) возвращает все добавленные в него символы и строки в одну строку.
+     */
+    public String paint(int width, int height) {
+        StringBuilder screen = new StringBuilder();
+        String ln = System.lineSeparator();
+        for (int i = 0; i < height; i++) {
+            for (int j = 0; j < width; j++) {
+                if ((i + j) % 2 == 0) {
+                    screen.append("x");
+                } else {
+                    screen.append(" ");
+                }
+            }
+            screen.append(ln);
+        }
+        return screen.toString();
+    }
+}
---
+++ src/test/java/ru/job4j/loop/BoardTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class BoardTest {
+    @Test
+    /**
+     * Тест: рисуем шахматную доску с шириной 3 и высотой 3.
+     */
+    public void whenPaintBoardWithWidthThreeAndHeightThreeThenStringWithThreeColsAndThreeRows() {
+        Board board = new Board();
+        String result = board.paint(3, 3);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x%s x %sx x%s", line, line, line);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    /**
+     * Тест: рисуем шахматную доску с шириной 5 и высотой 4.
+     */
+    public void whenPaintBoardWithWidthFiveAndHeightFourThenStringWithFiveColsAndFourRows() {
+        Board board = new Board();
+        String result = board.paint(5, 4);
+        final String line = System.getProperty("line.separator");
+        String expected = String.format("x x x%s x x %sx x x%s x x %s", line, line, line, line);
+        assertThat(result, is(expected));
+    }
+}
