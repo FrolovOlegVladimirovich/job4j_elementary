f0526fbdb55a1952044884aaf7e0522363d69e17
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-05-29 18:58:25 +0300

    3. Расстояние между точками в трехмерном пространстве.[#139329]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -4,22 +4,37 @@ package ru.job4j.condition;
  * Вычисление расстояния между точками в системе координат.
  * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
  * @since 09.05.2019
- * @version 2.0
+ * @version 3.0
  */
 public class Point {
     private int x;
     private int y;
+    private int z;
 
     /**
      * Конструктор класса Point.
      * Присваивает координаты точке.
      *
      * @param first координата точки по оси x.
-     * @param first координата точки по оси y.
+     * @param second координата точки по оси y.
      */
     public Point (int first, int second) {
         this.x = first;
         this.y = second;
+    }
+
+    /**
+     * Перегружаем конструктор класса Point на прием трех координат x y z.
+     * Присваивает координаты точке в трехмерном пространстве.
+     *
+     * @param x координата точки по оси x.
+     * @param y координата точки по оси y.
+     * @param z координата точки по оси z.
+     */
+    public Point (int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
     }
 
     /**
@@ -35,6 +50,19 @@ public class Point {
         double first = Math.pow(this.x - that.x, 2);
         double second = Math.pow(this.y - that.y, 2);
         return Math.sqrt(first + second);
+    }
+
+    /**
+     * Метод для вычисление расстояния между двумя точками в системе координат xyz.
+     *
+     * Math.pow(a, b) - возведение числа а в степень b.
+     * Math.sqrt(a) - извлечение квадратного корня.
+     *
+     * @param that координаты точки 2.
+     * @return Расстояние между точками 1 и 2 в системе координат xyz.
+     */
+    public double distance3d (Point that) {
+        return Math.sqrt(Math.pow(that.x - this.x, 2) + Math.pow(that.y - this.y, 2) + Math.pow(that.z - this.z, 2));
     }
 
     public void info() {
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -18,6 +18,15 @@ public class PointTest {
     }
 
     @Test
+    public void whenTwoPointsX3Y2Z3AndX1Y4Z4Then3() {
+        Point first = new Point(3, 2, 3);
+        Point second = new Point(1, 4, 4);
+        double result = first.distance3d(second);
+        System.out.println(String.format("Result is %s", result));
+        assertThat(result, is(3D));
+    }
+
+    @Test
     public void whenCheckItself() {
         Point point = new Point(0, 0);
         double result = point.distance(point);
