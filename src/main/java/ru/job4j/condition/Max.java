9781dd2a412dfd420e10e68e690c29ce8a8555ea
FrolovOlegVladimirovich <frolovolegvladimirovich@gmail.com>
2019-05-29 20:06:52 +0300

    4. Перегрузить метод max для трех чисел[#139330]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -4,11 +4,18 @@ package ru.job4j.condition;
  * Нахождение максимального числа из двух через тернарный оператор.
  * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
  * @since 11.05.2019
- * @version 1.0
+ * @version 2.0
  */
 public class Max {
-    public int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public int max(int one, int two) {
+        return one > two ? one : two;
+    }
+
+    public int max(int one, int two, int three) {
+        return (max(one, max(two, three)));
+    }
+
+    public int max(int one, int two, int three, int four) {
+        return max(one, max(two, three, four));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -8,7 +8,7 @@ import static org.junit.Assert.assertThat;
  * Тест на программу вычисления максимального числа из двух.
  * @author Oleg Frolov (frolovolegvladimirovich@gmail.com)
  * @since 11.05.2019
- * @version 1.0
+ * @version 2.0
  */
 public class MaxTest {
 
@@ -55,5 +55,172 @@ public class MaxTest {
         Max max = new Max();
         int result = max.max(2, 2);
         assertThat(result, is(2));
+    }
+
+    @Test
+    /**
+     * Тест для трех чисел.
+     *
+     * Если три числа равны.
+     * @param one первое число == 2.
+     * @param two второе число == 2.
+     * @param three третье число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three.
+     * @param is Ожидаемый результат 2.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To2To2Then2() {
+        Max max = new Max();
+        int result = max.max(2, 2, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    /**
+     * Тест для трех чисел.
+     *
+     * Если первое число максимальное.
+     * @param one первое число == 4.
+     * @param two второе число == 2.
+     * @param three третье число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax4To2To2Then4() {
+        Max max = new Max();
+        int result = max.max(4, 2, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    /**
+     * Тест для трех чисел.
+     *
+     * Если второе число максимальное.
+     * @param one первое число == 2.
+     * @param two второе число == 4.
+     * @param three третье число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To4To2Then4() {
+        Max max = new Max();
+        int result = max.max(2, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    /**
+     * Тест для трех чисел.
+     *
+     * Если третье число максимальное.
+     * @param one первое число == 2.
+     * @param two второе число == 2.
+     * @param three третье число == 4.
+     * @param result Результат вычисления максимального числа из one, two, three.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To2To4Then4() {
+        Max max = new Max();
+        int result = max.max(2, 2, 4);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    /**
+     * Тест для четырех чисел.
+     *
+     * Если четыре числа равны.
+     * @param one первое число == 2.
+     * @param two второе число == 2.
+     * @param three третье число == 2.
+     * @param four четвертое число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three, four.
+     * @param is Ожидаемый результат 2.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To2To2To2Then2() {
+        Max max = new Max();
+        int result = max.max(2, 2, 2, 2);
+        assertThat(result, is(2));
+    }
+
+    @Test
+    /**
+     * Тест для четырех чисел.
+     *
+     * Если первое число максимальное.
+     * @param one первое число == 4.
+     * @param two второе число == 2.
+     * @param three третье число == 2.
+     * @param four четвертое число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three, four.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax4To2To2To2Then4() {
+        Max max = new Max();
+        int result = max.max(4, 2, 2, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    /**
+     * Тест для четырех чисел.
+     *
+     * Если второе число максимальное.
+     * @param one первое число == 2.
+     * @param two второе число == 4.
+     * @param three третье число == 2.
+     * @param four четвертое число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three, four.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To4To2To2Then4() {
+        Max max = new Max();
+        int result = max.max(2, 4, 2, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    /**
+     * Тест для четырех чисел.
+     *
+     * Если третье число максимальное.
+     * @param one первое число == 2.
+     * @param two второе число == 2.
+     * @param three третье число == 4.
+     * @param four четвертое число == 2.
+     * @param result Результат вычисления максимального числа из one, two, three, four.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To2To4To2Then4() {
+        Max max = new Max();
+        int result = max.max(2, 2, 4, 2);
+        assertThat(result, is(4));
+    }
+
+    @Test
+    /**
+     * Тест для четырех чисел.
+     *
+     * Если четвертое число максимальное.
+     * @param one первое число == 2.
+     * @param two второе число == 2.
+     * @param three третье число == 2.
+     * @param four четвертое число == 4.
+     * @param result Результат вычисления максимального числа из one, two, three, four.
+     * @param is Ожидаемый результат 4.
+     * @param assertThat Результат сравнения result и is.
+     */
+    public void whenMax2To2To2To4Then4() {
+        Max max = new Max();
+        int result = max.max(2, 2, 4, 4);
+        assertThat(result, is(4));
     }
 }
